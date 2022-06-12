440883a39643cf9b165276a33750238cda3af1a0
Pierre-Elie Abergel <pe.abergel@gmail.com>
2022-06-12 12:35:32 +0200

    bootstrap ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@popperjs/core"
+import * as bootstrap from "bootstrap"
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
